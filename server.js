7ac4c73b2ca53b792dc0d07faa47c82c527a3924
Alex <alex.osadchuk.98@gmail.com>
2022-02-18 22:13:19 +0200

    maybe last

--- server.js
+++ server.js
@@ -7,6 +7,4 @@ app.use(express.static(__dirname + '/dist/steam-app'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/steam-app/index.html'));
 });
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Node app is running at localhost:" + app.get('port'));
-});
+app.listen(process.env.PORT || 8080);
