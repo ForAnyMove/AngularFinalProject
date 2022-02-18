ef0760ce2d13b6c70a90cfea95de8cf30e079124
Alex <alex.osadchuk.98@gmail.com>
2022-02-18 21:18:27 +0200

    heroku

--- server.js
+++ server.js
@@ -7,4 +7,6 @@ app.use(express.static(__dirname + '/dist/steam-app'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/steam-app/index.html'));
 });
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
